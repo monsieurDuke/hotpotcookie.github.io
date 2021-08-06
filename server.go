6723a9787a91de6f0f28c51b2410fd400df8aeec
aster-max <icatmuhammad2@gmail.com>
2021-08-06 23:26:58 +0700

    desiging card layout

--- server.go
+++ server.go
@@ -11,11 +11,13 @@ func main() {
     mux := http.NewServeMux()    
     log.Println("[gfarms]: starting server on",url)
     //    
+    mux.Handle("/asset", http.StripPrefix("/asset", http.FileServer(http.Dir("asset"))))        
+    mux.Handle("/source/css/", http.StripPrefix("/source/css/", http.FileServer(http.Dir("source/css"))))
+    mux.Handle("/source/js/", http.StripPrefix("/source/js/", http.FileServer(http.Dir("source/js"))))        
+    //    
     mux.HandleFunc("/",golang.HomePage)    
     mux.HandleFunc("/dashboard",golang.DboardPage)    
     //
-    mux.Handle("/source/css/", http.StripPrefix("/source/css/", http.FileServer(http.Dir("source/css"))))
-    mux.Handle("/source/js/", http.StripPrefix("/source/js/", http.FileServer(http.Dir("source/js"))))    
     err := http.ListenAndServe(":8080",mux)
     log.Fatal(err)
 }
