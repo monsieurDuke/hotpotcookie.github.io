058d0ba2c3ff018270ddc0a035e69b8780dd51b1
aster-max <icatmuhammad2@gmail.com>
2021-08-17 22:01:07 +0700

    adding more route to pages

--- source/golang/router.go
+++ source/golang/router.go
@@ -31,8 +31,8 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
     log.Println("[gfarms]: req HTTP/1.1 "+url+"/","|",r.RemoteAddr)
 }
 
-func DboardPage(w http.ResponseWriter, r *http.Request) {
-    tmp,err := template.ParseFiles(path.Join("pages","dashboard.html"))
+func TutorialPage(w http.ResponseWriter, r *http.Request) {
+    tmp,err := template.ParseFiles(path.Join("pages","tutorial.html"))
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         log.Println("[gfarms]: req HTTP/"+strconv.Itoa(http.StatusInternalServerError),err,"|",r.RemoteAddr)
@@ -43,5 +43,35 @@ func DboardPage(w http.ResponseWriter, r *http.Request) {
         log.Println("[gfarms]: req HTTP/"+strconv.Itoa(http.StatusInternalServerError),err,"|",r.RemoteAddr)        
         return
     }    
-    log.Println("[gfarms]: req HTTP/1.1 "+url+"/dashboard","|",r.RemoteAddr)
+    log.Println("[gfarms]: req HTTP/1.1 "+url+"/tutorial","|",r.RemoteAddr)
+}
+
+func ArticlePage(w http.ResponseWriter, r *http.Request) {
+    tmp,err := template.ParseFiles(path.Join("pages","article.html"))
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        log.Println("[gfarms]: req HTTP/"+strconv.Itoa(http.StatusInternalServerError),err,"|",r.RemoteAddr)
+        return        
+    }
+    err = tmp.Execute(w,nil)
+    if err != nil {
+        log.Println("[gfarms]: req HTTP/"+strconv.Itoa(http.StatusInternalServerError),err,"|",r.RemoteAddr)        
+        return
+    }    
+    log.Println("[gfarms]: req HTTP/1.1 "+url+"/article","|",r.RemoteAddr)
+}
+
+func PublicationPage(w http.ResponseWriter, r *http.Request) {
+    tmp,err := template.ParseFiles(path.Join("pages","publication.html"))
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        log.Println("[gfarms]: req HTTP/"+strconv.Itoa(http.StatusInternalServerError),err,"|",r.RemoteAddr)
+        return        
+    }
+    err = tmp.Execute(w,nil)
+    if err != nil {
+        log.Println("[gfarms]: req HTTP/"+strconv.Itoa(http.StatusInternalServerError),err,"|",r.RemoteAddr)        
+        return
+    }    
+    log.Println("[gfarms]: req HTTP/1.1 "+url+"/publication","|",r.RemoteAddr)
 }
